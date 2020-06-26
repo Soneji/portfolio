b52a658f230b3077681bc3e1bf819c28f0873a0b
Dhaval Soneji <dhaval@soneji.xyz>
2020-06-26 20:25:36 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -269,6 +269,7 @@ export default {
           { attr: 'owner.login', values: /lena/i, revert: true, more: '' },
           { attr: 'name', values: 'kcsoc_audiobooks', revert: true, more: '' },
           { attr: 'name', values: 'overclockedllama.github.io', revert: true, more: '' },
+          { attr: 'name', values: 'portfolio', revert: true, more: '' },
 
 
 
