936278c5340d286f13b4eba325d9d8a66435662c
Dhaval Soneji <dhaval@soneji.xyz>
2020-07-01 00:36:14 +0100

    add pics support

--- core/modules/github/repositories.ts
+++ core/modules/github/repositories.ts
@@ -1,17 +1,18 @@
-import config from '../../../config';
-import Cache from '../../classes/Cache';
-import Filter from '../../classes/Filter';
-import Sort from '../../classes/Sort';
-import { IGithubRepository } from '../../interfaces/IGithub';
-import variables from '../../variables';
-import Github from './Github';
+import config from "../../../config";
+import Cache from "../../classes/Cache";
+import Filter from "../../classes/Filter";
+import Sort from "../../classes/Sort";
+import { IGithubRepository } from "../../interfaces/IGithub";
+import variables from "../../variables";
+import Github from "./Github";
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 /**
  * @return {Promise<IGithubRepository[]>}
  * @throws
  */
 export default async (): Promise<IGithubRepository[]> => {
-
   const github = config.modules.github;
 
   const cache = new Cache(variables.cache.github.repositories);
@@ -21,7 +22,6 @@ export default async (): Promise<IGithubRepository[]> => {
   let repositories: IGithubRepository[];
 
   if (cache.needParse) {
-
     repositories = await Github.fetchRepositories();
 
     /*
@@ -29,10 +29,12 @@ export default async (): Promise<IGithubRepository[]> => {
      */
     cache.updateData(repositories);
     cache.updateTimestamp();
-
   } else {
     repositories = cache.dataFromFile;
-    Github.log(Github.sections.repositories, `Get from cache, ${repositories.length} length`).info();
+    Github.log(
+      Github.sections.repositories,
+      `Get from cache, ${repositories.length} length`
+    ).info();
   }
 
   /*
@@ -41,7 +43,10 @@ export default async (): Promise<IGithubRepository[]> => {
   const filter = new Filter(github.filter.repositories);
   if (filter.exists) {
     repositories = filter.run(repositories);
-    Github.log(Github.sections.repositories, `Filter, ${repositories.length} length`).info();
+    Github.log(
+      Github.sections.repositories,
+      `Filter, ${repositories.length} length`
+    ).info();
   }
 
   /*
@@ -53,6 +58,25 @@ export default async (): Promise<IGithubRepository[]> => {
       ? sort.desc(github.sort.repositories.attr)
       : sort.asc(github.sort.repositories.attr);
   }
-
+  for (let i = 0; i < repositories.length; i++) {
+    const repo = repositories[i];
+    // let url = `https://raw.githubusercontent.com/${repo.owner.login}/${repo.name}/master/.github/preview.jpg`;
+    let url = repo.html_url;
+    axios(url).then((response: { data: any }) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const metas = $("meta");
+      for (let i = 0; i < metas.length; i++) {
+        const meta = metas[i];
+        // console.log(meta.attribs);
+        if (meta.attribs.property == "og:image") {
+          let value = meta.attribs.content;
+          if (!value.includes("avatars")) {
+            repo.blobs_url = value;
+          }
+        }
+      }
+    });
+  }
   return repositories;
 };
