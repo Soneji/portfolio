754953ea30e82fac076b73c31b8163772e9fb4e1
Dhaval Soneji <dhaval@soneji.xyz>
2020-09-26 11:27:45 +0000

    Remove noor-gate.github.io

--- config.ts
+++ config.ts
@@ -288,6 +288,7 @@ export default {
           },
           { attr: "name", values: "portfolio", revert: true, more: "" },
           { attr: "name", values: "repo", revert: true, more: "" },
+          { attr: "name", values: "noor-gate.github.io", revert: true, more: "" },
         ],
         contributions: [
           { attr: "owner.login", values: "soneji", revert: true, more: "" },
