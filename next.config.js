2b3e71086ebcfae4f31f430d79bb11145a06f01a
Dhaval Soneji <dhaval@soneji.xyz>
2021-07-10 19:07:52 +0100

    Fix sharp

--- next.config.js
+++ next.config.js
@@ -18,7 +18,9 @@ module.exports = {
             test: /\.md$/,
             use: [{ loader: "html-loader" }, { loader: "markdown-loader" }],
         });
-
+        config.externals.push({
+            sharp: "commonjs sharp",
+        });
         return config;
     },
     target: "serverless",
